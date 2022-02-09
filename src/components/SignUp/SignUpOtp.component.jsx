4040619987331506b1ea011f25fcf89c550681e4
Russell Abdullah Al Mamun <joy.dtech@gmail.com>
2022-02-09 15:48:03 +0600

    off otp message

--- src/components/SignUp/SignUpOtp.component.jsx
+++ src/components/SignUp/SignUpOtp.component.jsx
@@ -3,63 +3,63 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory, useLocation } from "react-router";
 import { UserContext } from "../../App";
-import {
-  SIGNUP_OTP_CALL,
-  SIGNUP_RESEND_OTP_CALL,
-} from "../../requests/services";
+// import {
+//   SIGNUP_OTP_CALL,
+//   SIGNUP_RESEND_OTP_CALL,
+// } from "../../requests/services";
 import { showNotification } from "../../utils/notifications";
 import PrimaryButton from "../Buttons/PrimaryButton.component";
 import { OtpForm, SignInFormContainer } from "../SignIn/SignIn.style";
 
-function SignUpOtp({ userNum }) {
-  const { register, handleSubmit } = useForm();
-  const [otp, setOtp] = useState(new Array(4).fill(""));
-  let history = useHistory();
-  let location = useLocation();
-  let { from } = location.state || { from: { pathname: "/" } };
-  const { user, products } = useContext(UserContext);
-  const [loggedInUser, setLoggedInUser] = user;
+// function SignUpOtp({ userNum }) {
+//   const { register, handleSubmit } = useForm();
+//   const [otp, setOtp] = useState(new Array(4).fill(""));
+//   let history = useHistory();
+//   let location = useLocation();
+//   let { from } = location.state || { from: { pathname: "/" } };
+//   const { user, products } = useContext(UserContext);
+//   const [loggedInUser, setLoggedInUser] = user;
 
-  const handleChange = (element, index) => {
-    if (isNaN(element.value)) return false;
+//   const handleChange = (element, index) => {
+//     if (isNaN(element.value)) return false;
 
-    setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
+//     setOtp([...otp.map((d, idx) => (idx === index ? element.value : d))]);
 
-    if (element.nextSibling) {
-      element.nextSibling.focus();
-    }
-  };
+//     if (element.nextSibling) {
+//       element.nextSibling.focus();
+//     }
+//   };
 
-  const handleOtp = (e) => {
-    const currentOtp = { otp: otp.join(""), phone: userNum };
-    SIGNUP_OTP_CALL(currentOtp).then((response) => {
-      console.log(response);
-      if (response.data.errors) {
-        console.log(response.data);
-      } else {
-        setLoggedInUser(response.data.user);
-        localStorage["token"] = JSON.stringify(response.data.token);
-        setOtp([...otp.map((v) => "")]);
-        showNotification("Registered Successfully!");
-        history.replace(from);
-      }
-    });
-  };
+//   const handleOtp = (e) => {
+//     const currentOtp = { otp: otp.join(""), phone: userNum };
+//     SIGNUP_OTP_CALL(currentOtp).then((response) => {
+//       console.log(response);
+//       if (response.data.errors) {
+//         console.log(response.data);
+//       } else {
+//         setLoggedInUser(response.data.user);
+//         localStorage["token"] = JSON.stringify(response.data.token);
+//         setOtp([...otp.map((v) => "")]);
+//         showNotification("Registered Successfully!");
+//         history.replace(from);
+//       }
+//     });
+//   };
 
-  const handleResendOtp = (e) => {
-    if (userNum) {
-      const phone = { phone: userNum };
-      SIGNUP_RESEND_OTP_CALL(phone).then((response) => {
-        console.log(response);
-        if (response.data.errors) {
-          console.log(response);
-        } else {
-          setOtp([...otp.map((v) => "")]);
-          console.log("success");
-        }
-      });
-    }
-  };
+//   const handleResendOtp = (e) => {
+//     if (userNum) {
+//       const phone = { phone: userNum };
+//       SIGNUP_RESEND_OTP_CALL(phone).then((response) => {
+//         console.log(response);
+//         if (response.data.errors) {
+//           console.log(response);
+//         } else {
+//           setOtp([...otp.map((v) => "")]);
+//           console.log("success");
+//         }
+//       });
+//     }
+//   };
 
   return (
     <>
@@ -68,30 +68,30 @@ function SignUpOtp({ userNum }) {
         <br />
         <p>Please fill in the Otp:</p>
         <br />
-        <OtpForm onSubmit={handleSubmit(handleOtp)}>
-          <section>
-            {otp.map((data, index) => {
-              return (
-                <input
-                  className="otp-field"
-                  type="text"
-                  name="otp"
-                  maxLength="1"
-                  key={index}
-                  value={data}
-                  onChange={(e) => handleChange(e.target, index)}
-                  onFocus={(e) => e.target.select()}
-                />
-              );
-            })}
-          </section>
+//         <OtpForm onSubmit={handleSubmit(handleOtp)}>
+//           <section>
+//             {otp.map((data, index) => {
+//               return (
+//                 <input
+//                   className="otp-field"
+//                   type="text"
+//                   name="otp"
+//                   maxLength="1"
+//                   key={index}
+//                   value={data}
+// //                   onChange={(e) => handleChange(e.target, index)}
+// //                   onFocus={(e) => e.target.select()}a
+//                 />
+//               );
+//             })}
+//           </section>
           <br />
-          <p onClick={handleResendOtp}>Resend OTP</p>
+//           <p onClick={handleResendOtp}>Resend OTP</p>
           <br />
           {/* {error && <small>OTP did not matched! Try again.</small>} */}
-          <button type="submit">
-            <PrimaryButton>Submit Otp</PrimaryButton>
-          </button>
+//           <button type="submit">
+//             <PrimaryButton>Submit Otp</PrimaryButton>
+//           </button>
         </OtpForm>
       </SignInFormContainer>
     </>
